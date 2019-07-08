98d4c718373f00014ee094c2a1251c8352bc36d6
Gareth Slinn <gslinn@gareths-mbp.home>
2019-07-08 21:02:50 +0100

    add hooks to Tooltip

--- documentation/src/components/Tooltip.tsx
+++ documentation/src/components/Tooltip.tsx
@@ -1,8 +1,4 @@
-import * as React from 'react';
-
-interface State {
-  isVisible: boolean;
-}
+import React, {useState} from "react";
 
 interface Props {
   position: string;
@@ -12,7 +8,38 @@ interface Props {
   forceWidth: number;
 }
 
-export class Tooltip extends React.Component<Props, State> {
+export const Tooltip: React.FunctionComponent<Props> = (props:any) => {
+
+  const { type, children, message, position, forceWidth } = props;
+  const rangeWidth = forceWidth ? forceWidth + 'px' : 'auto';
+  const style = {
+    width: rangeWidth
+  };
+  const [activeState, setActive] = useState(false);
+  const show = (isActive: boolean) => {
+    setActive(isActive)
+  };
+
+  return (
+      <span className="tooltip" onMouseLeave={() => show(false)}>
+        {activeState && (
+            <div
+                className={`tooltip-content tooltip-${position} 
+                tooltip-${type}`}
+                style={style}
+            >
+              <div className="tooltip-message">{message}</div>
+            </div>
+        )}
+        <span className="tooltip-trigger" onMouseOver={() => show(true)}>
+          {children}
+        </span>
+      </span>
+  );
+};
+
+
+export class Tooltip1 extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
